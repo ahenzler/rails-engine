2025ac75ecad7c3d1c763166fd53fa84fd4055da
ahenzler <59706432+ahenzler@users.noreply.github.com>
2021-07-21 13:54:56 -0600

    add item model

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
 
-RSpec.describe , type: :model do
+RSpec.describe Merchant, type: :model do
   describe 'relationships' do
     it { should have_many(:items)}
-    it { should have_many(:invoices)}
+    # it { should have_many(:invoices)}
     # it { should have_many(:).through(:)}
   end
 
   describe 'validations' do
-    it { should validate_presence_of(:name)}
+    it { should validate_presence_of(:name) }
   end
 end
