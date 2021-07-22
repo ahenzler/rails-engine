67e1706bad18d015061459288e0a327c6ab188b5
ahenzler <59706432+ahenzler@users.noreply.github.com>
2021-07-22 14:28:22 -0600

    add items create and destroy

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,14 +1,34 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-    if params[:page].to_i >= 1
-      items = Item.paginate(page: params[:page], per_page: params[:per_page])
+    if params[:merchant_id]
+      Merchant.find(params[:merchant_id])
+      render json: ItemSerializer.new(Item.where(merchant_id: params[:merchant_id]))
     else
-      items = Item.paginate(page: "1", per_page: params[:per_page])
+      if params[:page].to_i >= 1
+        items = Item.paginate(page: params[:page], per_page: params[:per_page])
+      else
+        items = Item.paginate(page: "1", per_page: params[:per_page])
+      end
+      render json: (ItemSerializer.new(items)).serializable_hash
     end
-    render json: (ItemSerializer.new(items)).serializable_hash
   end
 
   def show
     render json: ItemSerializer.new(Item.find(params[:id]))
+  end
+
+  def create
+    Item.create!(item_params)
+    require 'pry'; binding.pry
+  end
+
+  def destroy
+    Item.destroy(params[:id])
+  end
+
+  private
+
+  def item_params
+    params.permit(:merchant_id, :name, :description, :unit_price)
   end
 end
--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -17,9 +17,25 @@ RSpec.describe 'items' do
       get api_v1_items_path(page: 2)
       expect(items.count).to be <= 20
     end
+
+    it 'has only one merchants items listed' do
+      merchant1 = create(:merchant)
+      merchant2 = create(:merchant)
+      create_list(:item, 51, merchant: merchant1)
+      create_list(:item, 53, merchant: merchant2)
+
+      get api_v1_merchant_items_path(merchant1.id)
+      expect(response).to be_successful
+
+      items = JSON.parse(response.body)
+      expect(items.count).to be <= 20
+
+      get api_v1_merchant_items_path(merchant1.id, page: 2)
+      expect(items.count).to be <= 20
+    end
   end
 
-    describe 'item#show' do
+  describe 'item#show' do
     it 'can get one item' do
       merchant1 = create(:merchant)
       item1 = create(:item, merchant: merchant1)
@@ -32,6 +48,35 @@ RSpec.describe 'items' do
       expect(item["data"]["attributes"]["name"]).to eq item1.name
       expect(item["data"]["attributes"]["merchant_id"]).to eq merchant1.id
       expect(item.count).to be == 1
+    end
+  end
+
+  describe 'item#create' do
+    it 'can create a item' do
+      merchant1 = create(:merchant)
+      expect(merchant1.items.count).to be == 0
+
+      post '/api/v1/items', params: {name: 'test', description: 'testing testing', unit_price: 11.11, merchant_id: merchant1.id}
+      expect(response).to be_successful
+      expect(merchant1.items.count).to be == 1
+      expect(merchant1.items.first.name).to eq 'test'
+      expect(merchant1.items.first.description).to eq 'testing testing'
+      expect(merchant1.items.first.unit_price).to eq 11.11
+      expect(merchant1.items.first.merchant_id).to eq merchant1.id
+    end
+  end
+
+  describe 'item#destroy' do
+    it 'can destroy a item' do
+      merchant1 = create(:merchant)
+      item1 = create(:item, merchant: merchant1)
+      expect(merchant1.items.count).to be == 1
+
+      delete api_v1_item_path(id: item1.id)
+
+      expect(response).to be_successful
+      expect(merchant1.items.count).to be == 0
+      expect(Item.find_by(id: item1.id)).to be nil
     end
   end
 end
